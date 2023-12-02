d6f4ff024d2cb44f533d5a21d7f3de52090cc4f5
void2610 <izumisan2610@gmail.com>
2023-12-02 15:10:23 +0900

    testプログラムとworkflowファイルを実装

---
+++ test/api_test.js
@@ -0,0 +1,41 @@
+import {
+  assertEquals,
+  assertObjectMatch,
+  assertStringIncludes,
+} from "https://deno.land/std/testing/asserts.ts";
+import { serve } from "https://deno.land/std/http/server.ts";
+
+const PORT = 8080;
+const BASE_URL = `http://localhost:${PORT}/api`;
+
+console.log(`Server is running at http://localhost:${PORT}`);
+
+function assertObjectMatchWithoutDtAndId(actual, expected) {
+  // dtとidを除いたオブジェクトが一致することを確認する
+  const { dt, id, ...actualWithoutDtAndId } = actual;
+  const { dt: expectedDt, id: expectedId, ...expectedWithoutDtAndId } = expected;
+
+  assertEquals(actualWithoutDtAndId, expectedWithoutDtAndId);
+}
+
+Deno.test("Post Tweet", async () => {
+  const response = await fetch(`${BASE_URL}/tweet/add`, {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify({ user: "testUser", content: "Test tweet" }),
+  });
+
+  assertEquals(response.status, 200);
+  const result = await response.json();
+  assertEquals(result, "ok");
+});
+
+Deno.test("Get Tweet List", async () => {
+  const response = await fetch(`${BASE_URL}/tweet/list`);
+  assertEquals(response.status, 200);
+  const result = await response.json();
+
+  assertObjectMatchWithoutDtAndId(result[0], {
+    value: { content: "Test tweet", user: "testUser" },
+  });
+});
